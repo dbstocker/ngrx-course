5f325493f573a5324074f77fb2672e9b731de594
dbstocker <coffeemetalcode@gmail.com>
2023-03-07 14:54:43 -0500

    finish section 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import {Observable} from "rxjs";
 import {map} from 'rxjs/operators';
 import {NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router} from '@angular/router';
 import { AuthState } from './auth/reducers';
-import { logout } from './auth/auth.actions';
+import { login, logout } from './auth/auth.actions';
 import { isLoggedIn, isLoggedOut } from './auth/auth.selectors';
 
 @Component({
@@ -25,6 +25,12 @@ export class AppComponent implements OnInit {
     }
 
     ngOnInit() {
+
+      const userProfile = localStorage.getItem('user');
+
+      if(userProfile) {
+        this.store.dispatch(login({user: JSON.parse(userProfile)}));
+      }
 
       this.router.events.subscribe(event  => {
         switch (true) {
